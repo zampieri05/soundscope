f43a6ecc6b6e5f34f9b15e4ae7fc32f28e4e9a86
Rodrigo Zampieri <ptrex10@gmail.com>
2026-09-21 21:18:18 +0000

    config: connect SoundScope Accounts to Cognito

---
+++ frontend/js/accounts-config.js
@@ -0,0 +1,8 @@
+window.SoundScopeAccountsConfig = Object.freeze({
+  region: "us-east-1",
+  userPoolId: "us-east-1_xXS3ZbBIZ",
+  clientId: "khsrn7u8qjge2l9rt5srdfgiu",
+  domain: "soundscope-15837-6080",
+  redirectUri: "https://d3pzcehppsvhm0.cloudfront.net/preview-v2/index.html",
+  logoutUri: "https://d3pzcehppsvhm0.cloudfront.net/preview-v2/index.html"
+});
